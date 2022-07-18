66fd966c21b46a509007e7dbe06c3931fc91f484
ved1150 <vppatel1771@gmail.com>
2022-07-18 06:10:16 +0530

    add obj

--- main.js
+++ main.js
@@ -344,40 +344,55 @@ const msg = document.querySelector('.msg');
 const userList = document.querySelector('#users');
 const submitBtn = document.querySelector("#submitBtn")
 // Listen for form submit
+// myForm.addEventListener('submit', onSubmit);
+
+
+
+submitBtn.addEventListener("click",function(){
+    let my_info ={
+      name : `${nameInput.value}`,
+      email : `${emailInput.value}`
+    }
+    my_info_serialized = JSON.stringify(my_info)
+    
+    localStorage.setItem("my_info", my_info_serialized  )
+    
+    // let my_info_desrialized = JSON.parse(localStorage.getItem("my_info"));
+    console.log(localStorage)
+    // localStorage.setItem("email",emailInput.value)
+})
+
+// Listen for form submit
 myForm.addEventListener('submit', onSubmit);
 
 function onSubmit(e) {
   e.preventDefault();
   
-//   if(nameInput.value === '' || emailInput.value === '') {
-//     // alert('Please enter all fields');
-//     msg.classList.add('error');
-//     msg.innerHTML = 'Please enter all fields';
+  if(nameInput.value === '' || emailInput.value === '') {
+    // alert('Please enter all fields');
+    msg.classList.add('error');
+    msg.innerHTML = 'Please enter all fields';
 
-//     // Remove error after 3 seconds
-//     setTimeout(() => msg.remove(), 3000);
-//   } else {
+    // Remove error after 3 seconds
+    setTimeout(() => msg.remove(), 3000);
+  } else {
 
-//     // localStorage.setItem(`${nameInput} , ${nameInput.value}`)
-//     // Create new list item with user
-//     const li = document.createElement('li');
+    // localStorage.setItem(`${nameInput} , ${nameInput.value}`)
+    // Create new list item with user
+    const li = document.createElement('li');
 
-//     // Add text node with input values
-//     li.appendChild(document.createTextNode(`${nameInput.value}: ${emailInput.value}`));
+    // Add text node with input values
+    li.appendChild(document.createTextNode(`${nameInput.value}: ${emailInput.value}`));
 
-//     localStorage.setItem(`name, ${nameInput.value}`)
-//     // // Add HTML
-//     // // li.innerHTML = `<strong>${nameInput.value}</strong>e: ${emailInput.value}`;
+    // localStorage.setItem(`name, ${nameInput.value}`)
+    // // Add HTML
+    li.innerHTML = `<strong>${nameInput.value}</strong>e: ${emailInput.value}`;
 
-//     // // Append to ul
-//     // userList.appendChild(li);
+    // // Append to ul
+    userList.appendChild(li);
 
-//     // Clear fields
-//     nameInput.value = '';
-//     emailInput.value = '';
+    // Clear fields
+    nameInput.value = '';
+    emailInput.value = '';
   }
-
-submitBtn.addEventListener("click",function(){
-    localStorage.setItem("name", nameInput.value)
-    localStorage.setItem("email",emailInput.value)
-})
+}
